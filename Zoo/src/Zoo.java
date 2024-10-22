9e1a63795c2f68d2878d82d08e1965a1619df8f7
unknown <azzatrk01@gmail.com>
2024-10-22 13:34:48 +0100

    prosit4

--- Zoo/src/Zoo.java
+++ Zoo/src/Zoo.java
@@ -94,4 +94,7 @@ public class Zoo {
         return "Zoo name: " + this.name + ", City: " + this.city + ", Number of cages: " + this.NBR_CAGES;
     }
 
-   }
+    public String getName() {
+        return this.name;
+    }
+}
